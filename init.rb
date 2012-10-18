f06c889bcd55ae0ca7b18eb751afc02239034593
Harold Giménez <harold.gimenez@gmail.com>
2012-10-18 16:19:35 -0700

    More to $:

--- init.rb
+++ init.rb
@@ -1,4 +1,5 @@
 $:.unshift File.join(File.dirname(__FILE__), 'vendor', 'keikokuc', 'lib')
+$:.unshift File.join(File.dirname(__FILE__), 'vendor', 'keikokuc')
 require './vendor/keikokuc/lib/keikokuc'
 require 'heroku/command/base'
 require 'heroku/command/notifications'
