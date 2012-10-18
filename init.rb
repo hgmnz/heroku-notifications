6343246189db3f96abbec7bfa9f456d9c36707b5
Harold Giménez <harold.gimenez@gmail.com>
2012-10-18 16:05:16 -0700

    Clean up

--- init.rb
+++ init.rb
@@ -1,4 +1,5 @@
 $:.unshift File.expand_path(File.join('vendor', 'keikokuc', 'lib'), File.dirname(__FILE__))
+puts $:
 require 'keikokuc'
 require 'heroku/command/base'
 require 'heroku/command/notifications'
