e574c77819e177b615c7ab197c837ae320f3561a
Harold Giménez <harold.gimenez@gmail.com>
2012-10-18 16:13:29 -0700

    Require relative

--- init.rb
+++ init.rb
@@ -1,5 +1,4 @@
 $:.unshift File.join(File.dirname(__FILE__), 'vendor', 'keikokuc', 'lib')
-puts $:
-require 'keikokuc'
+require './vendor/keikokuc/lib/keikokuc'
 require 'heroku/command/base'
 require 'heroku/command/notifications'
