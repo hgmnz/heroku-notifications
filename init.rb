b3785b59d6d35b8ba5f6298c550cbd73ae221f4e
Harold Giménez <harold.gimenez@gmail.com>
2012-10-19 18:52:31 -0700

    Fix loadpath again

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-$:.unshift File.join(File.dirname(__FILE__), 'vendor', 'keikokuc', 'lib', 'keikokuc')
+$:.unshift File.join(File.dirname(__FILE__), 'vendor', 'keikokuc', 'lib')
 require 'keikokuc'
 require 'heroku/command/base'
 require 'heroku/command/notifications'
